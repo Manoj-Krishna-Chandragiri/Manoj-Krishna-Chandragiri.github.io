d149fd01fe3b211e04709441cb9d161fe3ffc048
Manoj-Krishna-Chandragiri <manojkrishnachandragiri@gmail.com>
2025-05-05 07:08:13 +0530

    Fix dependency issues and update vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-  server: {
-    fs: {
-      strict: false
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom']
-  }
 })
